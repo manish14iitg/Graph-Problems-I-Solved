26e9fd801ca3c281a15c93afd37c2dbf813b0f08
manish14iitg <111654062+manish14iitg@users.noreply.github.com>
2024-10-11 18:13:55 +0530

    Create CycleDetection_Undirected_Graph_DFS.cpp

---
+++ CycleDetection_Undirected_Graph_DFS.cpp
@@ -0,0 +1,34 @@
+bool DetectCycle(int node,int parent, vector<int> adj[], vector<bool>&visited){
+        visited[node] = 1;
+        for(int i = 0; i< adj[node].size(); i++){
+            if(parent == adj[node][i])
+            continue;
+            if(visited[adj[node][i]]){
+                return 1;
+            }
+            if(DetectCycle(adj[node][i],node,adj,visited)){
+                return 1;
+            }
+        }
+        return 0;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        // using DFS
+        // recursive call for every node with a parent, 
+        //if neighoubor is parent it is actually returning 
+        //to where it came, so will skip this consition
+        // and if neighobor is already visited, cycle is detected
+        // and if no cycle is detected for any neighbor it will return 0;
+        // also for not connected graph we have to check this for every
+        // part of the graph by a loop;
+        
+        vector<bool>visited(V,0);
+        for(int i = 0; i< V; i++){
+            if(visited[i] == 0){
+                if(DetectCycle(i,-1,adj,visited))
+                return 1;
+            }
+        }
+        return 0;
+    }
